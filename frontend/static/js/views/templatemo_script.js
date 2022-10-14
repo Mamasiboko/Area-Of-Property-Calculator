7e20bfe23993416aa3fac33f10f8d3d19a6d4ca4
Mamasiboko <113340653+Mamasiboko@users.noreply.github.com>
2022-10-14 07:47:55 +0200

    Update templatemo_script.js

--- frontend/static/js/views/templatemo_script.js
+++ frontend/static/js/views/templatemo_script.js
@@ -4,8 +4,7 @@ function onWindowResize()
 	var container = document.getElementById('home');
 	var navbar = document.getElementById('navbar');
 	var content = document.getElementById('thumbnails_container');
-	// container.removeChild(navbar);
-	// container.insertBefore(navbar, content);		
+		
 }
 
 $(document).ready(function(){
